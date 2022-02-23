f74cfb6c03c4c237786ae23f30d50a1213c6ec59
Tiernan8r <tiernan8r@pm.me>
2022-02-23 10:32:54 +0000

    Implement the `tensor_product()` method:
    Method takes in two matrices and computes the tensor product as a larger
    matrix, and returns that matrix.
    
    The tensor product of two matrices A, B is defined as:
    
    A x B = [ A00 * B, A01 * B, ... A0n * B ]
    	[ A10 * B, A11 * B, ...	A1n * B ]
    	[ ...			...	]
    	[ An0 * B, An1 * B, ... Ann * B ]
    
    Resulting in an (mp)*(np) matrix, if A is m*n & B is n*p.
    
    The code works by iterating over the indices of the full mp*np matrix,
    and populating the entries. To calculate the factor of A to use, the
    current indices are floor divided by the width of A (= n). This factor
    is then multiplied against the corresponding B entry, which is
    calculated by taking the modulus of the indices.
    
    Calculting the tensor product in this way results in an O(n^2)
    calculation.

--- qcp/tensor_product.py
+++ qcp/tensor_product.py
@@ -12,7 +12,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from matrix import Matrix
+from square_matrix import SquareMatrix
 
 
-def tensor_product(a: Matrix, b: Matrix):
-    raise NotImplementedError()
+def tensor_product(A: Matrix, B: Matrix):
+    # A = m * n matrix
+    # B = n * p
+    m = len(A)
+    n = len(A[0])
+    p = len(B[0])
+
+    row_width = n * m
+    column_width = n * p
+
+    # creates an (m*n)*(n*p) list for the answer matrix
+
+    # entries = [[None] * row_width] * column_width
+    entries = make_zeros(row_width, column_width)
+    # print("ENTRIES:")
+    # print(entries)
+
+    # A * B =
+    # [A00 * B, A01 * B, ... A0n * B]
+    # [A10 * B, A11 * B, ... A1n * B]
+    # [ ...              ...        ]
+    # [An0 * B, A1n * B, ... Ann * B]
+
+    for i in range(row_width):
+        for j in range(column_width):
+
+            # A[k][l]:
+            k = i // n
+            l = j // n
+
+            # B[p][q]
+            p = i % n
+            q = j % n
+
+            # print(f"@({i},{j}): A=({k},{l}) / B=({p},{q})")
+
+            entries[i][j] = A[k][l] * B[p][q]
+            # print(entries)
+
+    return SquareMatrix(entries)
+
+
+def make_zeros(n, m):
+    l = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(0)
+        l.append(row)
+    return l
