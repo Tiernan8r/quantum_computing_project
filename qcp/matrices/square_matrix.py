17d678e6a494e2e82b40b8161c8d57df8e88ecc9
Tiernan8r <tiernan8r@pm.me>
2022-02-23 19:15:41 +0000

    tests for all methods in square_matrix.py

--- qcp/matrices/square_matrix.py
+++ qcp/matrices/square_matrix.py
@@ -24,13 +24,23 @@ class SquareMatrix(Matrix):
         return len(self._state)
 
     def __getitem__(self, i: int) -> List[Union[complex, float]]:
+        assert i < len(self), "index out of range"
         return self._state[i]
 
-    def set_state(self, s: List[List[Union[complex, float]]]):
-        self._state = s
+    def __setitem__(self, i: int, v: List[Union[complex, float]]):
+        assert i < len(self), "index out of range"
+        assert len(v) == len(self), "row dimension does not match"
+
+        self._state[i] = v
 
     def get_state(self) -> List[List[Union[complex, float]]]:
         return self._state
+
+    def set_state(self, s: List[List[Union[complex, float]]]):
+        assert s is not None
+        assert len(s) > 0
+        assert len(s) == len(s[0]), "non square matrix state"
+        self._state = s
 
     def rows(self) -> List[List[Union[complex, float]]]:
         """Return the rows of the Matrix."""
@@ -39,12 +49,12 @@ class SquareMatrix(Matrix):
     def columns(self) -> List[List[Union[complex, float]]]:
         """Returns the columns of the Matrix"""
         return [
-            [self._state[j][i] for i in range(len(self))]
+            [self._state[i][j] for i in range(len(self))]
             for j in range(len(self))
         ]
 
     def __iter__(self):
-        return self.get_state()
+        return iter(self.get_state())
 
     def __add__(self, other: Matrix) -> Matrix:
         assert len(self) == len(other) and len(self[0]) == len(
@@ -59,7 +69,7 @@ class SquareMatrix(Matrix):
         return SquareMatrix(current_state)
 
     def __sub__(self, other: Matrix) -> Matrix:
-        return self + (-1 * other)
+        return self + (other * -1)
 
     def __mul__(self, other: Union[complex, float, int, Matrix]) -> Matrix:
 
@@ -76,12 +86,12 @@ class SquareMatrix(Matrix):
             return self._dot(other)
 
     def _dot(self, other: Matrix) -> Matrix:
-        current_state = self.get_state().copy()
-        cols = other.columns()
+        n = len(self)
+        current_state = [[0 for i in range(n)] for j in range(n)]
 
-        for i in range(len(self)):
-            for j in range(len(cols)):
-                current_state[i][j] = self[i][j] * cols[i][j]
+        for i in range(n):
+            for j in range(n):
+                current_state[i][j] = sum([self[i][k] * other[k][j] for k in range(n)])
 
         return SquareMatrix(current_state)
 
---
+++ tests/matrices/test_square_matrix.py
@@ -0,0 +1,218 @@
+# Copyright 2022 Tiernan8r
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from copy import deepcopy
+from urllib.request import build_opener
+from qcp.matrices import SquareMatrix
+import pytest
+
+
+IDENTITY = SquareMatrix([[1, 0], [0, 1]])
+TEST_ONE_BY_ONE = SquareMatrix([[1]])
+TEST_TWO_BY_TWO = SquareMatrix([[1, 2], [3, 4]])
+TEST_THREE_BY_THREE = SquareMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+
+def test_sq_m_init():
+    input = [[1]]
+
+    sqm = SquareMatrix(input)
+
+    assert sqm.get_state() == input
+
+
+def test_sq_m_len():
+    assert len(TEST_ONE_BY_ONE) == 1
+    assert len(TEST_TWO_BY_TWO) == 2
+    assert len(TEST_THREE_BY_THREE) == 3
+
+
+def test_sq_m_get_item():
+    assert TEST_ONE_BY_ONE[0][0] == 1
+    assert TEST_TWO_BY_TWO[1][1] == 4
+    assert TEST_THREE_BY_THREE[2][2] == 9
+
+    with pytest.raises(AssertionError) as ae:
+        _ = TEST_TWO_BY_TWO[3][3]
+    assert ae.match("index out of range")
+
+
+def test_sq_m_set_item():
+    one_by_one = deepcopy(TEST_ONE_BY_ONE)
+    one_by_one[0] = [0]
+    assert one_by_one[0][0] == 0
+
+    two_by_two = deepcopy(TEST_TWO_BY_TWO)
+    two_by_two[1] = [5, 6]
+    assert two_by_two[1][1] == 6
+
+    with pytest.raises(AssertionError) as ae1:
+        two_by_two[2] = [5, 6]
+    assert ae1.match("index out of range")
+
+    with pytest.raises(AssertionError) as ae2:
+        two_by_two[0] = [1, 2, 3, 4]
+    assert ae2.match("row dimension does not match")
+
+
+def test_sq_m_get_state():
+    assert TEST_ONE_BY_ONE.get_state() == [[1]]
+    assert TEST_TWO_BY_TWO.get_state() == [[1, 2], [3, 4]]
+
+
+def test_sq_m_set_state():
+    one_by_one = deepcopy(TEST_ONE_BY_ONE)
+
+    with pytest.raises(AssertionError) as ae1:
+        one_by_one.set_state(None)
+    with pytest.raises(AssertionError) as ae2:
+        one_by_one.set_state([])
+
+    with pytest.raises(AssertionError) as ae3:
+        one_by_one.set_state([[1], [2, 3]])
+    assert ae3.match("non square matrix")
+
+    one_by_one.set_state([[1, 2], [3, 4]])
+    assert one_by_one.get_state() == [[1, 2], [3, 4]]
+
+
+def test_sq_m_rows():
+    assert TEST_ONE_BY_ONE.rows() == [[1]]
+    assert TEST_TWO_BY_TWO.rows() == [[1, 2], [3, 4]]
+
+
+def test_sq_m_columns():
+    assert TEST_ONE_BY_ONE.columns() == [[1]]
+    assert TEST_TWO_BY_TWO.columns() == [[1, 3], [2, 4]]
+    assert TEST_THREE_BY_THREE.columns() == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+
+def test_sq_m_iter():
+    build_1x1 = []
+    for r1 in TEST_ONE_BY_ONE:
+        build_1x1.append(r1)
+    expected_1x1 = [[1]]
+
+    assert build_1x1 == expected_1x1
+
+    build_2x2 = []
+    for r2 in TEST_TWO_BY_TWO:
+        build_2x2.append(r2)
+    expected_2x2 = [[1, 2], [3, 4]]
+
+    assert build_2x2 == expected_2x2
+
+    build_3x3 = []
+    for r3 in TEST_THREE_BY_THREE:
+        build_3x3.append(r3)
+    expected_3x3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    assert build_3x3 == expected_3x3
+
+
+def test_sq_m_add():
+    A1x1 = SquareMatrix([[1]])
+    B1x1 = SquareMatrix([[2]])
+    C1x1 = SquareMatrix([[3]])
+
+    assert (A1x1 + B1x1).get_state() == C1x1.get_state()
+
+    A2x2 = SquareMatrix([[1, 2], [3, 4]])
+    B2x2 = SquareMatrix([[5, 6], [7, 8]])
+    C2x2 = SquareMatrix([[6, 8], [10, 12]])
+
+    assert (A2x2 + B2x2).get_state() == C2x2.get_state()
+
+
+def test_sq_m_sub():
+    A1x1 = SquareMatrix([[2]])
+    B1x1 = SquareMatrix([[1]])
+    C1x1 = SquareMatrix([[1]])
+
+    assert (A1x1 - B1x1).get_state() == C1x1.get_state()
+
+    A2x2 = SquareMatrix([[5, 6], [7, 8]])
+    B2x2 = SquareMatrix([[1, 2], [3, 4]])
+    C2x2 = SquareMatrix([[4, 4], [4, 4]])
+
+    assert (A2x2 - B2x2).get_state() == C2x2.get_state()
+
+
+def test_sq_m_mul_scalar():
+    # Testing with ints:
+    A1x1 = SquareMatrix([[1]])
+    B1x1 = SquareMatrix([[2]])
+
+    assert (A1x1 * 2).get_state() == B1x1.get_state()
+
+    A2x2 = SquareMatrix([[1, 2], [3, 4]])
+    B2x2 = SquareMatrix([[3, 6], [9, 12]])
+
+    assert (A2x2 * 3).get_state() == B2x2.get_state()
+
+    # Testing with floats:
+    A1x1 = SquareMatrix([[1.0]])
+    B1x1 = SquareMatrix([[0.5]])
+
+    assert (A1x1 * 0.5).get_state() == B1x1.get_state()
+
+    A2x2 = SquareMatrix([[1.5, 2.5], [3.5, 4.5]])
+    B2x2 = SquareMatrix([[2.25, 3.75], [5.25, 6.75]])
+
+    assert (A2x2 * 1.5).get_state() == B2x2.get_state()
+
+    # Testing with complex:
+    A1x1 = SquareMatrix([[1]])
+    B1x1 = SquareMatrix([[1 + 0j]])
+
+    assert (A1x1 * (1+0j)).get_state() == B1x1.get_state()
+
+    A2x2 = SquareMatrix([[1 + 1j, 2 + 2j], [3 + 3j, 4 + 4j]])
+    B2x2 = SquareMatrix([[2 + 2j, 4 + 4j], [6 + 6j, 8 + 8j]])
+
+    assert (A2x2 * 2).get_state() == B2x2.get_state()
+
+    C2x2 = SquareMatrix([[1 + 1j, 2 + 2j], [3 + 3j, 4 + 4j]])
+    D2x2 = SquareMatrix([[4j, 8j], [12j, 16j]])
+
+    assert (C2x2 * (2+2j)).get_state() == D2x2.get_state()
+
+
+def test_sq_m_mul_dot_product():
+    A1x1 = SquareMatrix([[2]])
+    B1x1 = SquareMatrix([[3]])
+    C1x1 = SquareMatrix([[6]])
+
+    assert (A1x1 * B1x1).get_state() == C1x1.get_state()
+
+    A2x2 = SquareMatrix([[1, 2], [3, 4]])
+    B2x2 = SquareMatrix([[5, 6], [7, 8]])
+    C2x2 = SquareMatrix([[19, 22], [43, 50]])
+
+    assert (A2x2 * B2x2).get_state() == C2x2.get_state()
+
+    A3x3 = SquareMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    B3x3 = SquareMatrix([[10, 11, 12], [13, 14, 15], [16, 17, 18]])
+    C3x3 = SquareMatrix([[84, 90, 96], [201, 216, 231], [318, 342, 366]])
+
+    assert (A3x3 * B3x3).get_state() == C3x3.get_state()
+
+def test_sq_m_str():
+    expected1x1 = "[ 1]"
+    assert str(TEST_ONE_BY_ONE) == expected1x1
+
+    expected2x2 = "[ 1, 2]\n[ 3, 4]"
+    assert str(TEST_TWO_BY_TWO) == expected2x2
+
+    expected3x3 = "[ 1, 2, 3]\n[ 4, 5, 6]\n[ 7, 8, 9]"
+    assert str(TEST_THREE_BY_THREE) == expected3x3
