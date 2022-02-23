3d225ab8c03928c449286a46be06c7a8eeba2e31
Jabeth <J.Musumba@sms.ed.ac.uk>
2022-02-23 14:13:26 +0000

    Implemented Hadamard matrix

--- qcp/gates.py
+++ qcp/gates.py
@@ -1,10 +1,19 @@
 from Matrix import Matrix
+import consts
+from tensor_product import tensor_product
 
 
-def hadamards(nBits, targets):
-    ret = Matrix()
+def hadamard(size, targets):
+    m = consts.IDENTITY
 
-    return ret
+    for i in range(size):
+        if i in targets:
+            m = tensor_product(m, consts.TWO_HADAMARD)
+        else:
+            m = tensor_product(m, consts.IDENTITY)
+
+    h = Matrix(m.get_state())
+    return h
 
 # TODO: ORACLE - AKA Controlled z Gate
 # TODO: X Gate
