f33fad1c885c257a017a752ed93da8333b0fdb8d
Jabeth <J.Musumba@sms.ed.ac.uk>
2022-02-23 14:40:57 +0000

    Implemented dummy matrix class and constants class

---
+++ qcp/constants.py
@@ -0,0 +1,8 @@
+import matrix
+
+IDENTITY = matrix([[1, 0], [0, 1]])
+TWO_HADAMARD = matrix([[1, 1], [1, -1]])
+ZERO_VECTOR = matrix([[1], [0]])
+ONE_VECTOR = matrix([[0], [1]])
+PAULI_X = matrix([[0, 1], [1, 0]])
+PAULI_Z = matrix([[1, 0], [0, -1]])
---
+++ qcp/matrix.py
@@ -0,0 +1,6 @@
+
+class matrix:
+
+    def __init__(self, state):
+        self.state = state
+    
