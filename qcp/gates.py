ba4baf36564428e9d5a889a524c1a2c0d18ca4c8
Jabeth <J.Musumba@sms.ed.ac.uk>
2022-02-24 00:25:54 +0000

    Implemented multi_gates functions
    multi_x - a function which applies a Pauli-X gate on all the qubits specified in the params
    
    multi_z - a function which applies a Pauli-Z gate on all the qubits specified in the params

--- qcp/gates.py
+++ qcp/gates.py
@@ -2,23 +2,55 @@ from matrix import matrix
 import constants as c
 from tensor_product import tensor_product
 
+'''
+Multi_Gates: constructs a large circuit matrix which acts as a gate that applies a specific gate to one or more qubits 
+Params:
+    size - total number of qubits we're working with
+    targets - the specific qubits the gate is applied to (starting from the 0th qubit)
+'''
 
-def hadamard(size, targets):
-    m = c.IDENTITY
+
+def multi_hadamard(size, targets):
+    h = matrix([[1]])
 
     for i in range(size):
         if i in targets:
-            m = tensor_product(m, c.TWO_HADAMARD)
+            h = tensor_product(h, c.TWO_HADAMARD)
         else:
-            m = tensor_product(m, c.IDENTITY)
+            h = tensor_product(h, c.IDENTITY)
 
-    h = matrix(m.get_state())
     return h
 
 
-def control_x():
+def multi_x(size, targets):
+    x = matrix([1])
+
+    for i in range(size):
+        if i in targets:
+            x = tensor_product(x, c.PAULI_X)
+        else:
+            x = tensor_product(x, c.IDENTITY)
+    return x
+
+
+def multi_z(size, targets):
+    z = matrix([1])
+
+    for i in range(size):
+        if i in targets:
+            z = tensor_product(z, c.PAULI_Z)
+        else:
+            z = tensor_product(z, c.IDENTITY)
+    return z
+
+
+def control_x(size, control, target):
     pass
 
 
-def control_z():
+def control_z(size, control, target):
+    pass
+
+
+def toffoli(size, control1, control2, target):
     pass
