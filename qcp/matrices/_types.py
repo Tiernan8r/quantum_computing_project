2de83694af8880489eae49c9c97d9b7b22f84b99
Tiernan8r <tiernan8r@pm.me>
2022-02-25 15:43:52 +0000

    whitespaces and licenses

--- qcp/matrices/_types.py
+++ qcp/matrices/_types.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Dict, List,  Union
+from typing import Dict, List, Union
 
 SCALARS = Union[complex, float, int]
 SCALARS_TYPES = (complex, float, int)
