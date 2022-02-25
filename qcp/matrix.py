dceb631d24e0e4dea3d1853b885552451abe21c8
JebM <63006029+JabethM@users.noreply.github.com>
2022-02-25 14:34:17 +0000

    Implement gates (#26)
    * Implemented dummy matrix class and constants class
    
    * Implemented Hadamard Gate and dummy tensor product function
    
    * Implemented multi_gates functions
    
    multi_x - a function which applies a Pauli-X gate on all the qubits specified in the params
    
    multi_z - a function which applies a Pauli-Z gate on all the qubits specified in the params
    
    * implemented cnot gate
    
    * Implemented control z gate/ fixed potential notation errors with target qubits
    
    * Implemented phase shift and control phase shift gates + normalisation for hadamard constant
    
    * Implemented Toffoli Gate
    
    * Generalised the multi_gate functions
    
    * Implemented multiple control usage on control gates, removing the need for Toffoli gate
    
    * Improved readability
    
    * Suggested Changes for readability
    
    * Wrapping Docstrings to be under 80 characters
    
    * pep8 fix

---
+++ qcp/matrix.py
@@ -0,0 +1,22 @@
+class Matrix:
+
+    def __init__(self, state):
+        pass
+
+    def __len__(self):
+        pass
+
+    def get_state(self):
+        pass
+
+    def set_state(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
