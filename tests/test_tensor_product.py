8fa1cffc62c989b40f95a417b754b45f6dc32a53
Tiernan8r <tiernan8r@pm.me>
2022-03-03 17:33:09 +0000

    tidy tests ad tensor_product fixes raised in the tests

--- tests/test_tensor_product.py
+++ tests/test_tensor_product.py
@@ -11,15 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-<<<<<<< HEAD
 import qcp.tensor_product as tp
-from qcp.matrices import DenseMatrix, SparseMatrix
+from qcp.matrices import DefaultMatrix, DenseMatrix, SparseMatrix
+from qcp.tensor_product import tensor_product
 
 IDENTITY = SparseMatrix.identity(2)
-=======
-from qcp.tensor_product import tensor_product
-from qcp.matrices import DefaultMatrix
->>>>>>> remove .DS_Store
 
 
 def test_tensor_product_dense_with_identity():
@@ -162,32 +158,31 @@ def test_tensor_sparse():
     assert F.get_state() == expected2.get_state()
 
 
-def tensor_test_smallnonsquare():
+def test_tensor_product_non_square():
 
-    # nonsquare matrices to be incorparted into the below function
-    ret = True
+    mat1_1 = DefaultMatrix([[5, 1]])
+    mat1_2 = DefaultMatrix([[9, 4]])
+    mat1_3 = DefaultMatrix([[5, 3, 1],
+                            [4, 9, 1]])
 
-    M1 = [[[5, 1]],
-          [[9, 4]],
-          [[5, 3, 1], [4, 9, 1]]]
+    mat2_1 = DefaultMatrix([[6, 8]])
+    mat2_2 = DefaultMatrix([[7, 2]])
+    mat2_3 = DefaultMatrix([[7, 1, 2],
+                            [2, 5, 3]])
 
-    M2 = [[[6, 8]],
-          [[7, 2]],
-          [[7, 1, 2], [2, 5, 3]]]
+    ans1 = DefaultMatrix([[30, 40, 6, 8]])
+    ans2 = DefaultMatrix([[63, 18, 28, 8]])
+    ans3 = DefaultMatrix([[35, 5, 10, 21, 3, 6, 7, 1, 2],
+                          [10, 25, 15, 6, 15, 9, 2, 5, 3],
+                          [28, 4, 8, 63, 9, 18, 7, 1, 2],
+                          [8, 20, 12, 18, 45, 27, 2, 5, 3]])
 
-    Ans = [[[30, 40, 6, 8]],
-           [[35, 5, 10, 21, 3, 6, 7, 1, 2], [10, 25, 15, 6, 15, 9, 2, 5, 3], [
-               28, 4, 8, 63, 9, 18, 7, 1, 2], [8, 20, 12, 18, 45, 27, 2, 5, 3]],
-           [[63, 18, 28, 8]]]
-
-    for n in range(0, len(Ans)):
-        ret = ret and (tensor_product(M1[n], M2[n]) == Ans[n]) and (
-            tensor_product(transpose(M1[n]), transpose(M2[n])) == transpose(Ans[n]))
-
-    return ret
+    assert tp.tensor_product(mat1_1, mat2_1).get_state() == ans1.get_state()
+    assert tp.tensor_product(mat1_2, mat2_2).get_state() == ans2.get_state()
+    assert tp.tensor_product(mat1_3, mat2_3).get_state() == ans3.get_state()
 
 
-def tensor_test_square():
+def test_tensor_product_square():
 
     # ret => variable to return
     ret = True
@@ -196,80 +191,83 @@ def tensor_test_square():
     # each M1 matrix has the operation applied to the corresponding M2 matrix
     # the result of the tensor product function is then compared with the seperatly calculated answer key stored in Ans
     M1 = [
-         [[2, 3],
-          [5, 3]],
+        [[2, 3],
+         [5, 3]],
 
-         [[6, 5],
-          [3, 1]],
+        [[6, 5],
+         [3, 1]],
 
-         [[3.6, 9.8],
-          [2.1, 5.3]],
+        [[3.6, 9.8],
+         [2.1, 5.3]],
 
-         [[7.6, 8.0],
-          [complex(3, 2), 2.4]]
+        [[7.6, 8.0],
+         [complex(3, 2), 2.4]],
 
-         [[4, 6, 9],
-          [0, 1, 0],
-          [0, 6, 7]]]
+        [[4, 6, 9],
+         [0, 1, 0],
+         [0, 6, 7]]
+    ]
 
     M2 = [
-         [[4, 6],
-          [3, 4]],
+        [[4, 6],
+         [3, 4]],
 
-         [[4, 9],
-          [3, 7]],
+        [[4, 9],
+         [3, 7]],
 
-         [[7.6, 8.0],
-          [9.9, 2.4]],
+        [[7.6, 8.0],
+         [9.9, 2.4]],
 
-         [[complex(1, 1), 9.8],
-          [2.1, complex(0, 2)]],
+        [[complex(1, 1), 9.8],
+         [2.1, complex(0, 2)]],
 
         [[5, 7, 8],
-             [2, 1, 0],
-             [9, 2, 1]]]
+         [2, 1, 0],
+         [9, 2, 1]]
+    ]
 
     Ans = [
         [[8, 12, 12, 18],
-            [6, 8, 9, 12],
-            [20, 30, 12, 18],
-            [15, 20, 9, 12]],
+         [6, 8, 9, 12],
+         [20, 30, 12, 18],
+         [15, 20, 9, 12]],
 
         [[24, 54, 20, 45],
-            [18, 42, 15, 35],
-            [12, 27, 4, 9],
-            [9, 21, 3, 7]],
+         [18, 42, 15, 35],
+         [12, 27, 4, 9],
+         [9, 21, 3, 7]],
 
         [[27.36, 28.8, 74.48, 78.4],
-            [35.64, 8.64, 97.02, 23.52],
-            [15.96, 16.8, 40.28, 42.4],
-            [20.79, 5.04, 52.47, 12.72]],
+         [35.64, 8.64, 97.02, 23.52],
+         [15.96, 16.8, 40.28, 42.4],
+         [20.79, 5.04, 52.47, 12.72]],
 
         [[complex(7.6, 7.6), complex(8, 8), 74.48, 78.4],
-            [complex(1, 5), complex(2.4, 2.4), complex(29.4, 19.6), 23.52],
-            [15.96, 16.8, complex(0, 15.2), complex(0, 16)],
-            [complex(6.3, 4.2), 5.04, complex(-4, 6), complex(0, 4.8)]],
+         [complex(1, 5), complex(2.4, 2.4), complex(29.4, 19.6), 23.52],
+         [15.96, 16.8, complex(0, 15.2), complex(0, 16)],
+         [complex(6.3, 4.2), 5.04, complex(-4, 6), complex(0, 4.8)]],
 
         [[20, 28, 32, 30, 42, 48, 45, 63, 72],
-            [8, 4, 0, 12, 6, 0, 18, 9, 0],
-            [36, 8, 4, 54, 12, 6, 81, 18, 9],
-            [0, 0, 0, 5, 7, 8, 0, 0, 0],
-            [0, 0, 0, 2, 1, 0, 0, 0, 0],
-            [0, 0, 0, 9, 2, 1, 0, 0, 0],
-            [0, 0, 0, 30, 42, 48, 35, 49, 56],
-            [0, 0, 0, 12, 6, 0, 14, 7, 0],
-            [0, 0, 0, 54, 12, 6, 63, 14, 7]]]
+         [8, 4, 0, 12, 6, 0, 18, 9, 0],
+         [36, 8, 4, 54, 12, 6, 81, 18, 9],
+         [0, 0, 0, 5, 7, 8, 0, 0, 0],
+         [0, 0, 0, 2, 1, 0, 0, 0, 0],
+         [0, 0, 0, 9, 2, 1, 0, 0, 0],
+         [0, 0, 0, 30, 42, 48, 35, 49, 56],
+         [0, 0, 0, 12, 6, 0, 14, 7, 0],
+         [0, 0, 0, 54, 12, 6, 63, 14, 7]]
+    ]
 
     # tests all matrices with thier answers
     # taking the transpose of the matrices doubles the number of tests
     for n in range(0, len(Ans)):
-        ret = ret and (tensor_product(M1[n], M2[n]) == Ans[n]) and (
-            tensor_product(transpose(M1[n]), transpose(M2[n])) == transpose(Ans[n]))
+        ret = ret and (tp.tensor_product(M1[n], M2[n]) == Ans[n]) and (
+            tp.tensor_product(transpose(M1[n]), transpose(M2[n])) == transpose(Ans[n]))
 
     return ret
 
 
-def tensor_test_square_numbers():
+def test_tensor_product_square_numbers():
 
     ret = True
 
@@ -279,6 +277,6 @@ def tensor_test_square_numbers():
         for index in range(0, 10):
             Ques = [[index] * size]*size
             Ans = [[index**2]*(size**2)]*(size**2)
-            ret = ret and (tensor_product(Ques, Ques) == Ans)
+            ret = ret and (tp.tensor_product(Ques, Ques) == Ans)
 
     return ret
