55b16576d31a2ebc11c5f6dcbb5327ec6fc58d45
Tiernan8r <tiernan8r@pm.me>
2022-03-03 17:04:02 +0000

    remove .DS_Store

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-<<<<<<< HEAD
+from qcp.matrices import Matrix, DefaultMatrix
+import cmath
+from math import pi, sqrt
+from qcp.constants import TWO_HADAMARD
 from qcp.matrices import SparseMatrix
 import qcp.gates as gts
 import pytest
@@ -109,70 +112,54 @@ def test_control_phase():
 
 def test_phase_shift():
     pass
-=======
 
-from qcp.matrices import Matrix, DefaultMatrix
-import qcp.gates as gts
-from qcp.constants import TWO_HADAMARD
-from math import pi, sqrt
-import cmath
 
 def test_phase_gate():
 
-       ms = [ DefaultMatrix([[1,0],[0,1]]), 
-           DefaultMatrix([[1,0],[0,1j]]), 
-           DefaultMatrix([[1,0],[0,-1]]), 
-           DefaultMatrix([[1,0],[0,-1j]]) ] 
-       
-       assert cmath.isclose(gts.phase_shift(0)[1][1], ms[0][1][1])
-       assert cmath.isclose(gts.phase_shift(pi/2)[1][1], ms[1][1][1])
-       assert cmath.isclose(gts.phase_shift(pi)[1][1], ms[2][1][1])
-       assert cmath.isclose(gts.phase_shift(3*pi/2)[1][1], ms[3][1][1])
+    ms = [DefaultMatrix([[1, 0], [0, 1]]),
+          DefaultMatrix([[1, 0], [0, 1j]]),
+          DefaultMatrix([[1, 0], [0, -1]]),
+          DefaultMatrix([[1, 0], [0, -1j]])]
+
+    assert cmath.isclose(gts.phase_shift(0)[1][1], ms[0][1][1])
+    assert cmath.isclose(gts.phase_shift(pi/2)[1][1], ms[1][1][1])
+    assert cmath.isclose(gts.phase_shift(pi)[1][1], ms[2][1][1])
+    assert cmath.isclose(gts.phase_shift(3*pi/2)[1][1], ms[3][1][1])
+
 
 def test_hadamard_gate():
 
-       qubit0 = DefaultMatrix([[1],[0]])
-       qubit1 = DefaultMatrix([[0],[1]])
-       
-       ans0 = TWO_HADAMARD * qubit0
-       ans1 = TWO_HADAMARD * qubit1 
+    qubit0 = DefaultMatrix([[1], [0]])
+    qubit1 = DefaultMatrix([[0], [1]])
 
-       expected0 = (1/(sqrt(2))) * DefaultMatrix([[1],[1]])
-       expected1 = (1/(sqrt(2))) * DefaultMatrix([[1],[-1]])
+    ans0 = TWO_HADAMARD * qubit0
+    ans1 = TWO_HADAMARD * qubit1
 
-       assert expected0.get_state() == ans0.get_state()
-       assert expected1.get_state() == ans1.get_state()
+    expected0 = (1/(sqrt(2))) * DefaultMatrix([[1], [1]])
+    expected1 = (1/(sqrt(2))) * DefaultMatrix([[1], [-1]])
+
+    assert expected0.get_state() == ans0.get_state()
+    assert expected1.get_state() == ans1.get_state()
+
 
 def test_cnot_gate():
 
-       qubit0 = DefaultMatrix([[1],[0],[0],[0]])
-       qubit1 = DefaultMatrix([[0],[1],[0],[0]])
-       qubit2 = DefaultMatrix([[0],[0],[1],[0]])
-       qubit3 = DefaultMatrix([[0],[0],[0],[1]])
+    qubit0 = DefaultMatrix([[1], [0], [0], [0]])
+    qubit1 = DefaultMatrix([[0], [1], [0], [0]])
+    qubit2 = DefaultMatrix([[0], [0], [1], [0]])
+    qubit3 = DefaultMatrix([[0], [0], [0], [1]])
 
-       ans0 = gts.control_x(qubit0)
-       ans1 = gts.control_x(qubit1)
-       ans2 = gts.control_x(qubit2)
-       ans3 = gts.control_x(qubit3)
+    ans0 = gts.control_x(qubit0)
+    ans1 = gts.control_x(qubit1)
+    ans2 = gts.control_x(qubit2)
+    ans3 = gts.control_x(qubit3)
 
-       expected0 = DefaultMatrix([[1],[0],[0],[0]])
-       expected1 = DefaultMatrix([[0],[0],[0],[1]])
-       expected2 = DefaultMatrix([[0],[1],[0],[0]])
-       expected3 = DefaultMatrix([[0],[0],[1],[0]])
+    expected0 = DefaultMatrix([[1], [0], [0], [0]])
+    expected1 = DefaultMatrix([[0], [0], [0], [1]])
+    expected2 = DefaultMatrix([[0], [1], [0], [0]])
+    expected3 = DefaultMatrix([[0], [0], [1], [0]])
 
-       assert expected0.get_state() == ans0.get_state()
-       assert expected1.get_state() == ans1.get_state()
-       assert expected2.get_state() == ans2.get_state()
-       assert expected3.get_state() == ans3.get_state()
-
-
- 
-
-
-
-       
-
-
-
-
->>>>>>> finn-work
+    assert expected0.get_state() == ans0.get_state()
+    assert expected1.get_state() == ans1.get_state()
+    assert expected2.get_state() == ans2.get_state()
+    assert expected3.get_state() == ans3.get_state()
