edddd4ac50c31c4e53930db85dcdb7e7de24edbd
Jabeth <J.Musumba@sms.ed.ac.uk>
2022-03-09 13:39:52 +0000

    Basic class where the Grovers algorithm will take place

---
+++ qcp/grovers_algorithm.py
@@ -0,0 +1,16 @@
+import oracle
+
+
+class Grovers:
+    def __init__(self, size, target_state):
+        self.size = size
+        self.target = target_state
+
+    def construct_circuit(self):
+        pass
+
+    def run_algorithm(self):
+        pass
+
+    def probabilities(self):
+        pass
