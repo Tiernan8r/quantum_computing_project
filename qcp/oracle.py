7ed0931b71f6d094cd140a496e05ef25bd31fb1b
Jabeth <J.Musumba@sms.ed.ac.uk>
2022-03-09 11:40:09 +0000

    Changed indexing to start at 0

--- qcp/oracle.py
+++ qcp/oracle.py
@@ -4,9 +4,11 @@ import gates as g
 # size = number of qubits
 # target = specific numbered state we're going to look for
 def single_target_oracle(size, target):
-    not_placement = 2 ** size - target
+
+    not_placement = (2 ** size) - 1 - target
     t = pull_set_bits(not_placement)
-    cz = g.control_z(size, [x for x in range(1, size)], size)
+
+    cz = g.control_z(size, [x for x in range(0, size-1)], size-1)
     selector = g.multi_gate(size, t, 'x')
     oracle = selector * cz
     oracle *= selector
@@ -16,7 +18,7 @@ def single_target_oracle(size, target):
 # returns the list of bits that is set in number n
 def pull_set_bits(n):
     bits = []
-    count = 1
+    count = 0
     while n:
         cond = n & 1
         if cond:
@@ -26,4 +28,9 @@ def pull_set_bits(n):
     return bits
 
 
-
+def diffusion(size):
+    h = g.multi_gate(size, [i for i in range(0, size)], 'h')
+    cz = g.control_z(size, [i for i in range(0, size - 1)], size - 1)
+    x = g.multi_gate(size, [i for i in range(0, size)], 'x')
+    diff = h * (x * (cz * (x * h)))
+    return diff
