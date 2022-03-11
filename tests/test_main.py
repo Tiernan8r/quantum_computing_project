78413cfcdbff06ea3632cf2171097add7bcea96d
Tiernan8r <tiernan8r@pm.me>
2022-03-11 10:28:23 +0000

    add test on usage()

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,27 @@
 # limitations under the License.
 from qcp import main
 import pytest
+import sys
+
+
+def test_usage(capsys):
+    default_target = 5
+    expected_str = f"""USAGE:
+{sys.argv[0]} [FLAGS] nqbits
+
+ARGS:
+    nqbits          The number of qbit states to simulate
+FLAGS:
+    -t/--target     The target state, defaults to {default_target}
+    -h/--help       Display this prompt
+"""
+
+    with pytest.raises(SystemExit) as se:
+        main.usage()
+
+    assert se.match("0")
+    captured = capsys.readouterr()
+    assert captured.out == expected_str
 
 
 def test_main():
