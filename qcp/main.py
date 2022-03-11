bc7dd99057b32308c664e12ecc07079306a27b07
Tiernan8r <tiernan8r@pm.me>
2022-03-11 10:10:30 +0000

    change cli usage to not use click

--- qcp/main.py
+++ qcp/main.py
@@ -13,12 +13,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from qcp.grovers_algorithm import Grovers
-import click
+from typing import Tuple
+import sys
 
-@click.command()
-@click.option("-n", "--nqbits", default=5, help="Number of qbits in the system.")
-@click.option("-t", "--target", default=6, help="The qbit state to target")
-def main(nqbits: int, target: int):
+TARGET_DEF = 5
+USAGE_STR = f"""USAGE:
+{sys.argv[0]} [FLAGS] nqbits
+
+ARGS:
+    nqbits          The number of qbit states to simulate
+FLAGS:
+    -t/--target     The target state, defaults to {TARGET_DEF}
+    -h/--help       Display this prompt"""
+
+
+def main():
+    nqbits, target = parse_cli(sys.argv)
+    assert nqbits > 1, "must have a minimum of a 2 qbit state"
+
+    compute(nqbits, target)
+
+
+def usage():
+    print(USAGE_STR)
+    exit(0)
+
+
+def parse_cli(args) -> Tuple[int, int]:
+    num_args = len(args)
+    vals = []
+    targ = TARGET_DEF
+
+    # ignore the 1st arg as it is the program name
+    i = 1
+    while i < num_args:
+        arg = args[i]
+        if arg == "-h" or arg == "--help":
+            usage()
+        elif arg == "-t" or arg == "--target":
+            if i + 1 > num_args:
+                print("Must provide a target value!")
+                exit(1)
+            try:
+                targ = int(args[i+1])
+            except:
+                print("target state must be an integer")
+                exit(1)
+            i += 1
+        # Falls into the everything else category
+        else:
+            vals.append(arg)
+        i += 1
+
+    nqbits = 1
+    if len(vals) > 0:
+        try:
+            print(vals[0])
+            nqbits = int(vals[0])
+        except:
+            print("number of qbits must be an integer")
+            exit(1)
+    else:
+        print("Must provide the number of qbits to simulate")
+        exit(1)
+
+    return (nqbits, targ)
+
+
+def compute(nqbits: int, target: int):
     grover = Grovers(nqbits, target)
     grover.run()
 
