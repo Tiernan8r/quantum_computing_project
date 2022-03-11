5ad2a853ce23baa37284b3694b508979164740ff
tiernan8r <tiernan8r@pm.me>
2022-03-11 15:38:06 +0000

    changes to get the ui file working

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,28 +1,23 @@
-# This Python file uses the following encoding: utf-8
+# File: main.py
 import sys
-import os
-
-
-from PySide2.QtWidgets import QApplication, QWidget
-from PySide2.QtCore import QFile
-from PySide2.QtUiTools import QUiLoader
-
-
-class test(QWidget):
-    def __init__(self):
-        super(test, self).__init__()
-        self.load_ui()
-
-    def load_ui(self):
-        loader = QUiLoader()
-        path = os.path.join(os.path.dirname(__file__), "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
-        ui_file.close()
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
 
 if __name__ == "__main__":
-    app = QApplication([])
-    widget = test()
-    widget.show()
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+
+    ui_file_name = "form.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+    window.show()
+
+    sys.exit(app.exec())
