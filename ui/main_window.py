ecf06c3e9d5ff19e69d534e399c2ba2e09c09a9c
tiernan8r <tiernan8r@pm.me>
2022-03-11 15:12:52 +0000

    test changes

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,29 +1,28 @@
 # This Python file uses the following encoding: utf-8
-import os
-from pathlib import Path
 import sys
-
-from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
-from PySide6.QtUiTools import QUiLoader
+import os
 
 
-class MainWindow(QMainWindow):
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+
+class test(QWidget):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super(test, self).__init__()
         self.load_ui()
 
     def load_ui(self):
         loader = QUiLoader()
-        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
+        path = os.path.join(os.path.dirname(__file__), "form.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
         ui_file.close()
 
-
 if __name__ == "__main__":
     app = QApplication([])
-    widget = MainWindow()
+    widget = test()
     widget.show()
     sys.exit(app.exec_())
