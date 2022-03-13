7796a9ce608e09fe993fb1824196d0123bce4762
Tiernan8r <tiernan8r@pm.me>
2022-03-13 13:17:05 +0000

    use components in main_window.py

--- qcp/ui/main_window.py
+++ qcp/ui/main_window.py
@@ -5,6 +5,7 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtCore import QFile, QIODevice
 from qcp.ui.constants import UI_FILENAME
+import qcp.ui.components as comp
 
 
 class MainWindow(QMainWindow):
@@ -12,14 +13,24 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.ui_window = self.load_ui()
-        self.ui_window.setWindowTitle("Grover's Algorithm")
+        self.ui_component = self.load_ui()
+        self.ui_component.setWindowTitle("Grover's Algorithm")
+
+        self.graph_component = comp.GraphComponent(self)
+        self.input_component = comp.InputComponent(self)
+        self.button_component = comp.ButtonComponent(
+            self, self.input_component.input_search, self.input_component.input_target)
+
+        self.simulator = comp.SimulatorComponent(
+            self, self.button_component, self.graph_component)
 
     def show(self):
         # super().show()
-        self.ui_window.show()
+        self.ui_component.show()
 
-    def load_ui(self):
+    def load_ui(self) -> QMainWindow:
+
+        # Load in the 'form.ui' file where the ui layout is defined
         path = os.path.join(os.path.dirname(__file__), UI_FILENAME)
         ui_file = QFile(path)
 
@@ -27,6 +38,8 @@ class MainWindow(QMainWindow):
             print(f"Cannot open {UI_FILENAME}: {ui_file.errorString()}")
             sys.exit(-1)
 
+        # Load the file using the QT UI loader class and return the
+        # ui widget representing the layout
         loader = QUiLoader()
         ui_window = loader.load(ui_file)
         ui_file.close()
