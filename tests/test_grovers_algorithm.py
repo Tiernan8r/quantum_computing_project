d2e159c1e2350f0d7f20ecd4fd1a2df97647e608
Tiernan8r <tiernan8r@pm.me>
2022-03-14 17:36:34 +0000

    skeleton of test file

---
+++ tests/test_grovers_algorithm.py
@@ -0,0 +1,42 @@
+# Copyright 2022 Tiernan8r
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import qcp.grovers_algorithm as ga
+
+
+def test_init():
+    pass
+
+
+def test_initial_state():
+    pass
+
+
+def test_single_target_oracle():
+    pass
+
+
+def test_diffusion():
+    pass
+
+
+def test_construct_circuit():
+    pass
+
+
+def test_run():
+    pass
+
+
+def test_measure():
+    pass
