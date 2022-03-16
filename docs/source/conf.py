f6e96e93fd2c0ed9d711353e3d1bf624f6aa6ea4
Tiernan8r <tiernan8r@pm.me>
2022-03-16 16:15:44 +0000

    docstrings on dense_matrix.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,17 +31,24 @@ release = '0.0.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        "sphinx.ext.autodoc",
-        "sphinx.ext.napoleon",
-        "sphinx.ext.viewcode",
-        "sphinx.ext.autosummary",
-        "sphinx.ext.githubpages",
-        "sphinx.ext.autodoc",
-        "m2r2",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autodoc",
+    "m2r2",
 ]
 
 m2r_parse_relative_links = True
 m2r_anonymous_references = True
+autodoc_type_aliases = {
+    "SCALARS": "qcp.matrices.types.SCALARS",
+    "SCALARS_T": "qcp.matrices.types.SCALARS_T",
+    "VECTOR": "qcp.matrices.types.VECTOR",
+    "MATRIX": "qcp.matrices.types.MATRIX",
+    "SPARSE": "qcp.matrices.types.SPARSE",
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
