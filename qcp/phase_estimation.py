d5524c791551c0daf455a0c55160610af68a7a88
Tiernan8r <tiernan8r@pm.me>
2022-03-19 16:10:30 +0000

    autoformat phase_estimation.py

--- qcp/phase_estimation.py
+++ qcp/phase_estimation.py
@@ -1,4 +1,5 @@
-import math, cmath
+import math
+import cmath
 from qcp.matrices import DefaultMatrix
 import qcp.gates as g
 from qcp.tensor_product import tensor_product as tp
@@ -6,7 +7,7 @@ import qcp.register as reg
 import random
 
 
-def is_unitary (input: DefaultMatrix):
+def is_unitary(input: DefaultMatrix):
     """
     Check if matrix is Unitary (can be shifted to gates.py)
     :param DefaultMatrix: input: n x n matrix
@@ -15,17 +16,19 @@ def is_unitary (input: DefaultMatrix):
     identity = DefaultMatrix.identity(test.num_rows)
     for i in range(input.num_rows):
         for j in range(input.num_columns):
-            if cmath.isclose(test[i][j],identity[i][j]):
+            if cmath.isclose(test[i][j], identity[i][j]):
                 continue
             else:
                 return False
     return True
+
 
 def optimum_qubit_size(precision: int, error: float):
     """
     Returns number of qubit required for targeted number of decimal and error rate.
     """
     return math.ceil(precision + math.log2(2+1/(2*error)))
+
 
 def qft_gate(size: int):
     """
@@ -34,12 +37,14 @@ def qft_gate(size: int):
     :return DefaultMatrix: gate
     """
 
-    gate = g.multi_gate(size,[],g.Gate.I)
-    for i in range(size-1,-1,-1):
-        gate =  qft_rotation_gate(size,i)*g.multi_gate(size,[i],g.Gate.H)*gate
+    gate = g.multi_gate(size, [], g.Gate.I)
+    for i in range(size-1, -1, -1):
+        gate = qft_rotation_gate(
+            size, i)*g.multi_gate(size, [i], g.Gate.H)*gate
     for i in range(int(size/2)):
-        gate = g.swap(size,[i,size-1-i])*gate
-    return gate    
+        gate = g.swap(size, [i, size-1-i])*gate
+    return gate
+
 
 def inverse_qft_gate(size: int):
     """
@@ -47,12 +52,14 @@ def inverse_qft_gate(size: int):
     :param int: size: number of qubits
     :return DefaultMatrix: gate
     """
-    gate = g.multi_gate(size,[],g.Gate.I)
+    gate = g.multi_gate(size, [], g.Gate.I)
     for i in range(int(size/2)):
-        gate = g.swap(size, [i,size-i-1])*gate
-    for i in range(0,size):
-        gate =  g.multi_gate(size,[i],g.Gate.H)*inverse_qft_rotation_gate(size,i)*gate    
+        gate = g.swap(size, [i, size-i-1])*gate
+    for i in range(0, size):
+        gate = g.multi_gate(size, [i], g.Gate.H) * \
+            inverse_qft_rotation_gate(size, i)*gate
     return gate
+
 
 def qft_rotation_gate(size: int, current_qubit: int):
     """"
@@ -62,11 +69,12 @@ def qft_rotation_gate(size: int, current_qubit: int):
     :return DefaultMatrix: gate
     """
     gate = DefaultMatrix.identity(2**size)
-    for i in range (1,current_qubit+1):
+    for i in range(1, current_qubit+1):
         phi = 2*math.pi/2**(i+1)
         control = current_qubit-i
-        gate = gate * g.control_phase(size,[control],current_qubit,phi)
+        gate = gate * g.control_phase(size, [control], current_qubit, phi)
     return gate
+
 
 def inverse_qft_rotation_gate(size: int, current_qubit: int):
     """"
@@ -75,11 +83,12 @@ def inverse_qft_rotation_gate(size: int, current_qubit: int):
     :param current_qubit: which qubit to apply the rotation gate to, i 
     """
     gate = DefaultMatrix.identity(2**size)
-    for i in range(0,current_qubit):
+    for i in range(0, current_qubit):
         phi = -2*math.pi/2**(current_qubit+1-i)
         control = i
-        gate = gate * g.control_phase(size,[control],current_qubit,phi)
+        gate = gate * g.control_phase(size, [control], current_qubit, phi)
     return gate
+
 
 class phase_estimation:
 
@@ -116,32 +125,32 @@ class phase_estimation:
         """
         entries = [[0] for _ in range(2 ** self.size)]
         entries[0][0] = 1
-        return tp(self.auxiliary,DefaultMatrix(entries))
+        return tp(self.auxiliary, DefaultMatrix(entries))
 
     def first_layer(self):
         """
         Tensor Hadamard (first register) with Identity (auxiliary)
         """
-        return tp(g.multi_gate(self.auxsize,[],g.Gate.I),g.multi_gate(self.size,[i for i in range(self.size)],g.Gate.H))
+        return tp(g.multi_gate(self.auxsize, [], g.Gate.I), g.multi_gate(self.size, [i for i in range(self.size)], g.Gate.H))
 
     def second_layer(self):
         """
         Control-U Gate applied to auxiliary register
         """
         totalsize = self.size+self.auxsize
-        gate = g.multi_gate(totalsize,[],g.Gate.I)
+        gate = g.multi_gate(totalsize, [], g.Gate.I)
         rep = 0
-        for i in range(0,self.size):
+        for i in range(0, self.size):
             for j in range(2**rep):
-                gate = g.control_U(totalsize,self.size-1-i,self.unitary)*gate
-            rep += 1    
+                gate = g.control_U(totalsize, self.size-1-i, self.unitary)*gate
+            rep += 1
         return gate
 
     def third_layer(self):
         """"
         Inverse QFT Gate tensor for the first register
         """
-        return tp(g.multi_gate(self.auxsize,[],g.Gate.I),inverse_qft_gate(self.size))
+        return tp(g.multi_gate(self.auxsize, [], g.Gate.I), inverse_qft_gate(self.size))
 
     def construct_circuit(self):
         """
@@ -156,7 +165,7 @@ class phase_estimation:
         """
         self.state = self.circuit * self.state
         return self.state
-    
+
     def measure(self):
         """
         'measures' self.state by selecting a state weighted by its
@@ -168,15 +177,14 @@ class phase_estimation:
         for i in range(2**self.size):
             entries = [[0] for _ in range(2**self.size)]
             entries[i] = [1]
-            trial= tp(self.auxiliary,DefaultMatrix(entries))
+            trial = tp(self.auxiliary, DefaultMatrix(entries))
             result[i] = (trial.transpose()*self.state)[0]
 
         p = reg.measure(result)
         observed = random.choices([i for i in range(len(p))], p, k=1)
         probability = p[observed[0]]
         for i in range(2**self.size):
-             # print all the possiblities
-             # Should be removed, implement GUI histogram instead
-             print(i,round(p[i],4)) 
-        return observed[0]/2**self.size, round(probability,4)
-
+            # print all the possiblities
+            # Should be removed, implement GUI histogram instead
+            print(i, round(p[i], 4))
+        return observed[0]/2**self.size, round(probability, 4)
