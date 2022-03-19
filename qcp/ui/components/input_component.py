3731dec4e1f7982ed72bb7e20963c6ffd347707f
Tiernan8r <tiernan8r@pm.me>
2022-03-19 10:48:52 +0000

    Modify the UI input:
    Change the UI usage so that the first text box sets the number of qbits to
    simulate in our system, and the second one sets the target qbit.
    
    The code now runs the simulation of Grover's Algorithm and plots the result on
    the UI.
    
    If there is an error with the input, say we don't provide an integer for num
    qbits, then a red error text box will appear with the error message.
    
    Remove the browse file dialog as it is not needed for this iteration.

--- qcp/ui/components/input_component.py
+++ qcp/ui/components/input_component.py
@@ -11,10 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from multiprocessing.sharedctypes import Value
 from PySide6 import QtWidgets
 from qcp.ui.components import AbstractComponent
 from qcp.ui.constants import INPUT_SEARCH_WIDGET_NAME, \
-    INPUT_TARGET_WIDGET_NAME, INPUT_BROWSE_FILE
+    INPUT_TARGET_WIDGET_NAME, INPUT_SEARCH_ERROR_WIDGET_NAME, \
+    INPUT_TARGET_ERROR_WIDGET_NAME
+from typing import List
 
 
 class InputComponent(AbstractComponent):
@@ -35,53 +38,79 @@ class InputComponent(AbstractComponent):
         """
         super().__init__(main_window, *args, **kwargs)
 
+        self.input_search_error.hide()
+        self.input_target_error.hide()
+
     def setup_signals(self):
         """
         Setup the "browse" button to open the file dialog window when clicked.
         """
         super().setup_signals()
 
-        self.input_browse.clicked.connect(self.browse_files)
-
     def _find_widgets(self):
-        line_edits = self.main_window.ui_component.findChildren(
-            QtWidgets.QLineEdit)
-        for lineEdit in line_edits:
-            if lineEdit.objectName() == INPUT_TARGET_WIDGET_NAME:
-                self.input_target = lineEdit
+        line_edits: List[QtWidgets.QLineEdit] = \
+            self.main_window.ui_component.findChildren(
+                QtWidgets.QLineEdit
+        )
+        for line_edit in line_edits:
+            if line_edit.objectName() == INPUT_TARGET_WIDGET_NAME:
+                self.input_target = line_edit
+            elif line_edit.objectName() == INPUT_SEARCH_WIDGET_NAME:
+                self.input_search = line_edit
 
-        text_edit = self.main_window.ui_component.findChild(
-            QtWidgets.QTextEdit)
-        if text_edit is not None and \
-                text_edit.objectName() == INPUT_SEARCH_WIDGET_NAME:
-            self.input_search = text_edit
+        error_labels: List[QtWidgets.QLabel] = \
+            self.main_window.ui_component.findChildren(
+                QtWidgets.QLabel
+        )
+        for error_label in error_labels:
+            if error_label.objectName() == INPUT_SEARCH_ERROR_WIDGET_NAME:
+                self.input_search_error = error_label
+            elif error_label.objectName() == INPUT_TARGET_ERROR_WIDGET_NAME:
+                self.input_target_error = error_label
 
-        buttons = self.main_window.ui_component.findChildren(
-            QtWidgets.QPushButton)
-        for b in buttons:
-            if b.objectName() == INPUT_BROWSE_FILE:
-                self.input_browse = b
+    def _search_input_error(self, error: ValueError):
+        self.input_search_error.show()
 
-    def browse_files(self):
-        """
-        Create and display a QT file browser window to find files of
-        type `.txt`/`.json`/`.csv`/`.yml` and populate the search
-        box with the file's content.
-        """
-        dialog = QtWidgets.QFileDialog(self.main_window)
-        dialog.setFileMode(QtWidgets.QFileDialog.ExistingFile)
-        dialog.setNameFilter("Text (*.txt *.json *.csv *.yml)")
-        dialog.setViewMode(QtWidgets.QFileDialog.List)
+        self.input_search_error.setText(str(error))
 
-        file_names = []
-        if dialog.exec_():
-            file_names = dialog.selectedFiles()
 
-        if len(file_names) != 1:
-            return
+    def _target_input_error(self, error: ValueError):
+        self.input_target_error.show()
 
-        file_name = file_names[0]
+        self.input_target_error.setText(str(error))
 
-        with open(file_name, "r") as f:
-            content = f.read()
-            self.input_search.setPlainText(content)
+    def parse_input(self) -> int:
+        self.input_search_error.hide()
+
+        input_str = self.input_search.text()
+        input = 0
+        try:
+            input = int(input_str)
+        except ValueError as ve:
+            self._search_input_error(str(ve))
+            raise ve
+
+        if input < 2:
+            ve = ValueError("need a minimum of two qbits")
+            self._search_input_error(str(ve))
+            raise ve
+
+        return input
+
+    def parse_target(self, nqbits) -> int:
+        self.input_target_error.hide()
+
+        target_str = self.input_target.text()
+        target = 0
+        try:
+            target = int(target_str)
+        except ValueError as ve:
+            self._target_input_error(str(ve))
+            raise ve
+
+        if target < 0 or target > 2**nqbits:
+            ve = ValueError("target must be within qbit state size range")
+            self._target_input_error(str(ve))
+            raise ve
+
+        return target
--- qcp/ui/constants.py
+++ qcp/ui/constants.py
@@ -23,10 +23,14 @@ GRAPH_WIDGET_NAME = "graph"
 
 #: The widget name for the input search text box
 INPUT_SEARCH_WIDGET_NAME = "input_search"
+#: The widget name for the label to display any error message about search
+#: input
+INPUT_SEARCH_ERROR_WIDGET_NAME = "input_search_error"
 #: The widget name for the input target text box
 INPUT_TARGET_WIDGET_NAME = "input_target"
-#: The widget name for the button to launch the file browser
-INPUT_BROWSE_FILE = "browse_input_button"
+#: The widget name for the label to display any error message about
+#: target input
+INPUT_TARGET_ERROR_WIDGET_NAME = "input_target_error"
 
 #: The widget name for the button to initialise the search
 BUTTON_SEARCH_BUTTON = "search_button"
